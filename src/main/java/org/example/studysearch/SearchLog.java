4fb308c1e26bb9f6b0b1e4236755c9c65e7a3086
Hivna-Castro <hivna12@gmail.com>
2025-07-03 23:36:38 -0300

    fix: refactor code smell Feature Envy GeneralSearch using extract method refactoring technique

--- src/main/java/org/example/studysearch/SearchLog.java
+++ src/main/java/org/example/studysearch/SearchLog.java
@@ -1,5 +1,11 @@
 package org.example.studysearch;
 
+import org.example.studycards.CardManager;
+import org.example.studyplanner.HabitTracker;
+import org.example.studyplanner.TodoTracker;
+import org.example.studyregistry.StudyMaterial;
+import org.example.studyregistry.StudyTaskManager;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,18 +25,23 @@ public class SearchLog {
         numUsages = 0;
         isLocked = false;
     }
+
     public void addSearchHistory(String searchHistory) {
         this.searchHistory.add(searchHistory);
     }
+
     public List<String> getSearchHistory() {
         return searchHistory;
     }
+
     public void setSearchHistory(List<String> searchHistory) {
         this.searchHistory = searchHistory;
     }
+
     public Map<String, Integer> getSearchCount() {
         return searchCount;
     }
+
     public void setSearchCount(Map<String, Integer> searchCount) {
         this.searchCount = searchCount;
     }
@@ -57,5 +68,33 @@ public class SearchLog {
 
     public void setLogName(String logName) {
         this.logName = logName;
+    }
+
+    public void logSearch(String query) {
+        addSearchHistory(query);
+        setNumUsages(getNumUsages() + 1);
+    }
+
+    public String getFormattedLogInfo() {
+        return "\nLogged in: " + this.logName;
+    }
+
+    public List<String> searchWithLog(String text) {
+        List<String> results = SearchExecutor.execute(text);
+        logSearch(text);
+        results.add(getFormattedLogInfo());
+        return results;
+    }
+
+    public static class SearchExecutor {
+        public static List<String> execute(String text) {
+            List<String> results = new ArrayList<>();
+            results.addAll(CardManager.getCardManager().searchInCards(text));
+            results.addAll(HabitTracker.getHabitTracker().searchInHabits(text));
+            results.addAll(TodoTracker.getInstance().searchInTodos(text));
+            results.addAll(StudyMaterial.getStudyMaterial().searchInMaterials(text));
+            results.addAll(StudyTaskManager.getStudyTaskManager().searchInRegistries(text));
+            return results;
+        }
     }
 }
